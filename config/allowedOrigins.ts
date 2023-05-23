ee8c167454ece01aa7c984eeb3c2a97b5dafeaa2
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2023-05-23 19:57:04 -0300

    added one more url

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://127.0.0.1:5173", "http://127.0.0.1:8080", "http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5174", "http://127.0.0.1:8080", "http://localhost:5173"];
 
 export default allowedOrigins;
